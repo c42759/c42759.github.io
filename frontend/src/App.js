09850cce90d08e2cfc9b4c977e007c22c51458b0
Carlos Santos <carlos@one-shift.com>
2022-01-07 16:46:44 +0000

    chore: support multiple routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
-import Sidebar from './components/sidebar';
-import Main from './components/main';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import HomePage from './pages/home';
+import Weather from './pages/weather';
 
 function App() {
 	return (
-		<section className='App bg-gray-200  dark:bg-gray-700 text-gray-600 dark:text-gray-300 font-sans text-md'>
-			<div className='lg:max-w-screen-2xl lg:max-w-3/4 object-center mx-auto grid md:grid-cols-3'>
-				<Sidebar />
-				<Main />
-			</div>
-		</section>
+		<BrowserRouter>
+			<Switch>
+				<Route exact path='/' component={HomePage} />
+				<Route exact path='/weather/' component={Weather} />
+			</Switch>
+		</BrowserRouter>
 	);
 }
 
