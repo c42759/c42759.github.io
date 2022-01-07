14ea45944dacad7a386a18e904c94a89667af794
Carlos Santos <carlos@one-shift.com>
2022-01-07 16:48:53 +0000

    chore: improve educational item

---
+++ frontend/src/pages/home/components/educationItem.js
@@ -0,0 +1,16 @@
+import { React } from 'react';
+
+function EducationItem({ institution, course, duration, description }) {
+	return (
+		<div className='mb-10'>
+			<h5 className='uppercase font-semibold mb-2'>{institution}</h5>
+			<h6 className='uppercase mb-4'>
+				{course} | {duration.start_at} - {duration.end_at}
+			</h6>
+
+			<p className='mb-6'>{description}</p>
+		</div>
+	);
+}
+
+export default EducationItem;
