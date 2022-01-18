b55909f3623a79c58fdc0b1e16935a6b0f07fba1
Carlos Santos <carlos@one-shift.com>
2022-01-18 23:07:26 +0000

    chore: remove weather

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import HomePage from './pages/home'
-import Weather from './pages/weather'
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Switch>
 				<Route exact path='/' component={HomePage} />
-				<Route exact path='/weather/' component={Weather} />
 			</Switch>
 		</BrowserRouter>
 	);
